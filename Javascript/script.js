db31bf24a928561c989c332749632da7423d47df
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-12-26 20:28:11 -0300

    for com array

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,6 +2,16 @@ console.log("Input test");
 var nameInput = prompt("Input your name");
 console.log("Your name is " + nameInput);
 
-for (let i = 0; i < 10; i++){
+for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+var alunos = ["Mafalda", "Chiquinho", "Tina", "Léo", "Bruce", "Bolão", "Simi"];
+
+for(var aluno of alunos){
+    console.log(aluno)
+}
+for(var aluno in alunos){
+    console.log(aluno)
+}
+
