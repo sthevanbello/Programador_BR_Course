d49e297dbe4da5379ebdfb424613421418b05b1c
Sthevan Bello Alves <sthevan.alves@gmail.com>
2022-01-06 20:05:22 -0300

    Date JS

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,6 @@
-console.log("Input test");
-var nameInput = prompt("Input your name");
-console.log("Your name is " + nameInput);
+// console.log("Input test");
+// var nameInput = prompt("Input your name");
+// console.log("Your name is " + nameInput);
 
 for (let i = 0; i < 10; i++) {
     console.log(i);
@@ -9,9 +9,36 @@ for (let i = 0; i < 10; i++) {
 var alunos = ["Mafalda", "Chiquinho", "Tina", "Léo", "Bruce", "Bolão", "Simi"];
 
 for(var aluno of alunos){
-    console.log(aluno)
+    console.log(aluno);
 }
 for(var aluno in alunos){
-    console.log(aluno)
+    console.log(aluno);
 }
+
+var media = function (n1, n2){return (n1+n2)/2}; 
+console.log(media(2,5));
+
+var m = (n1, n2) => {return (n1+n2)/2};
+console.log(m(5,8));
+
+var aluno = {
+    nome: "Sthevan",
+    nota: [7.5, 5.0]
+};
+
+aluno.matricula = 123456;
+
+aluno["curso"] = "Analise e desenvolvimento de sistemas";
+
+var endereco = "endereco"
+
+aluno[endereco] = "Rua pastor João nunes, 146";
+
+console.log(typeof(aluno));
+console.log(aluno.nome);
+console.log(aluno.nota);
+console.log(aluno);
+
+var data = new Date();
+console.log(data);
 
