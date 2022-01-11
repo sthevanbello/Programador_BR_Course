2824ec140a0640aedcd76a56fd52c6f7bbbc506f
Sthevan Bello Alves <sthevan.alves@gmail.com>
2022-01-11 17:28:09 -0300

    Math

---
+++ Javascript/math/script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    let pi = Math.PI;
+    console.log(pi);
+
+    let e = Math.E;
+    console.log(e);
+
+    let sqrt = Math.sqrt(49);
+    console.log(sqrt);
+
+    let pow = Math.pow(5, 3);
+    console.log(pow);
+
+    let random = Math.random();
+    console.log(random);
+}
