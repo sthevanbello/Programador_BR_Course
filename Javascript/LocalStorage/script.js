6ded67e30fbff5e0a31768ee885a857d2ea4a65c
Sthevan Bello Alves <sthevan.alves@gmail.com>
2022-01-13 21:37:26 -0300

    To do list with javascript

--- Javascript/LocalStorage/script.js
+++ Javascript/LocalStorage/script.js
@@ -1,41 +1,92 @@
 // localStorage.setItem("nome", "Sthevan");
 // let nome = localStorage.getItem("nome");
 // console.log(nome);
-
 onload = function () {
-    let value = this.localStorage.getItem("name");
-    let campo = document.getElementById("campo");
-    campo.innerHTML = value;
-
     let button = document.getElementById("clear");
     button.addEventListener("click", clear);
 
     let insert = document.getElementById("insert");
     insert.addEventListener("click", insertTask);
+
+    let save = document.getElementById("btn-save");
+    save.addEventListener("click", saveList);
+
+    // let list = localStorage.getItem("item");
+    // console.log(JSON.stringify(list));
+    let list = localStorage.getItem("item");
+    console.log(list);
+
+    let load = document.getElementById("btn-load");
+    load.addEventListener("click", loadList);
+
 };
 
 function atualizar(element) {
-    // console.log(element.value);
-    let value = element.value;
-    let campo = document.getElementById("campo");
-    campo.innerHTML = value;
 
-    localStorage.setItem("name", value);
+    // let value = element.value;
+    // // let campo = document.getElementById("campo");
+    // // campo.innerHTML = value;
+
+    // localStorage.setItem("name", value);
 };
 
 
 function clear() {
-    let list = document.getElementById("list");
+    getList().innerHTML = "";
+    let task = document.getElementById("input");
+    task.value = "";
+    localStorage.clear();
+}
+
+function insertTask() {
+
+    let list = getList();
+    let text = document.getElementById("input").value
+    console.log(text);
+
+    let obj = {
+        "task": text
+    };
+    console.log(obj);
+
+    fillList(getList(), text);
+    saveList();
+}
+
+function fillList(list, value) {
+    console.log(value);
+    let li = createLi(value);
+    list.appendChild(li);
+}
+
+function createLi(value){
+    let li = document.createElement('li')
+    li.innerHTML = value;
+    return li
     
 }
 
-function insertTask(){
-    let task = document.getElementById("input");
-    // let line = {}
-    // localStorage.setItem("task", task);
-    let value = task.value;
-    console.log(value);
-    let list = document.getElementById("list");
-    list.innerHTML += "<li>" + value + "</li>";
+function saveList() {
+
+    let ul = getList();
+    console.log(ul);
+    localStorage.setItem("item", ul.innerHTML);
 }
 
+function getList() {
+    return document.getElementById("list");
+}
+
+
+function loadList() {
+    if (localStorage.length > 0) {
+        let list = getList();
+        list.innerHTML = "";
+        let storage = localStorage.getItem("item");
+        console.log(storage);
+        fillList(getList(), storage);
+    } else {
+        console.log("There isn't data in Local Storage")
+    }
+
+}
